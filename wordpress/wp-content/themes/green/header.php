b78dbfb215f8531f281451c231897fdebeba581a
Seth Hannah <SRHannah18@gmail.com>
2016-07-27 16:33:58 -0400

    viewport meta tag

--- wordpress/wp-content/themes/green/header.php
+++ wordpress/wp-content/themes/green/header.php
@@ -4,6 +4,7 @@
 	<head>
 		
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Green</title>
 		
 		<script type="text/javascript" src="/wordpress/wp-content/themes/green/js/fontfaceobserver.standalone.js"></script>
