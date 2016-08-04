fc83e51214f58a8c7b1bb49684680eaefc448795
Seth Hannah <SRHannah18@gmail.com>
2016-08-04 15:05:02 -0400

    hump

--- wp-content/themes/green/header.php
+++ wp-content/themes/green/header.php
@@ -7,8 +7,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Green</title>
 		
-		<script type="text/javascript" src="code/wp-content/themes/green/js/fontfaceobserver.standalone.js"></script>
-		<script type="text/javascript" src="code/wp-content/themes/green/js/green.js"></script>
+		<script type="text/javascript" src="/wordpress/wp-content/themes/green/js/fontfaceobserver.standalone.js"></script>
+		<script type="text/javascript" src="/wordpress/wp-content/themes/green/js/green.js"></script>
 		 
 		 
 
@@ -19,8 +19,9 @@
 		
 		<div class="header">
 			<div class="padbottom width">
-				<a href="http://sethhannah.com/green/"><img src="<?php header_image(); ?>" alt="header logo"></a>
+				<a href="http://sethhannah.com/green/wordpress"><img src="<?php header_image(); ?>" alt="header logo"></a>
 			</div>
+			
 
 	        <div class="textcenter">             
 	        	<h4>Green is a podcast about learning how to teach and teaching how to learn.</h4>
